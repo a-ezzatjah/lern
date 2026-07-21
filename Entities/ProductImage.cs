bad683b0eb25487faf20ff80b5a2af99cbc6443d
a-ezzatjah <a.ezzatjah@gmail.com>
2026-07-21 22:49:14 +0330

    add ProductImage and ProductVariant

---
+++ Entities/ProductImage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class ProductImage
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        // اختیاری: اگر عکس متعلق به یک رنگ یا تنوع خاص است
+        public int? VariantId { get; set; }
+        public ProductVariant Variant { get; set; }
+
+        public string ImageUrl { get; set; }
+        public string AltText { get; set; }
+
+        public int SortOrder { get; set; } // برای مشخص کردن ترتیب نمایش در گالری (0, 1, 2...)
+        public bool IsPrimary { get; set; } // آیا عکس اصلی/کارت محصول است؟
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Entities/ProductVariant.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class ProductVariant
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+        public Product product { get; set; }
+
+        public int ProductSaleOptionId { get; set; }
+        public ProductSaleOption ProductSaleOption { get; set; }
+
+        public int SaleOptionColorId { get; set; }
+        public SaleOptionColor optionColor { get; set; }
+
+
+        // اطلاعات انبارداری و قیمت
+        public string Sku { get; set; } // شناسه انبارداری یکتا (مثلا FABRIC-METRIC-RED)
+        public decimal Price { get; set; } // قیمت این تنوع خاص
+        public int StockQuantity { get; set; } // موجودی واقعی فیزیکی در انبار
+        public int ReservedQuantity { get; set; } // موجودی رزرو شده (مثلاً در سبد خریدهای معلق)
+
+
+
+        public int AvailableQuantity => StockQuantity - ReservedQuantity;
+
+
+
+
+
+
+    }
+}
