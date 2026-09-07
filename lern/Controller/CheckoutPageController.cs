5668f1d19669cad257e041ca1f8225d75bc45efe
a-ezzatjah <a.ezzatjah@gmail.com>
2026-09-07 23:44:45 +0330

    بررسی chek out

--- lern/Controller/CheckoutPageController.cs
+++ lern/Controller/CheckoutPageController.cs
@@ -2,9 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace lern.Controller;
 
-[Route("checkout")]
-public class CheckoutPageController : Microsoft.AspNetCore.Mvc.Controller
+public class CheckoutController : Microsoft.AspNetCore.Mvc.Controller
 {
-    [HttpGet]
     public IActionResult Index() => View();
 }
