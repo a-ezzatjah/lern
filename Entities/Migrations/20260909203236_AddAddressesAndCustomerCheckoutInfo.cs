b7fc57ed85ddd4f960b950b53f7e39f538bed514
a-ezzatjah <a.ezzatjah@gmail.com>
2026-09-10 00:12:44 +0330

    درست کردن مشکل صفحه ادیمن

---
+++ Entities/Migrations/20260909203236_AddAddressesAndCustomerCheckoutInfo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAddressesAndCustomerCheckoutInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerFirstName",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerLastName",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerPhone",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerFirstName",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerLastName",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerPhone",
+                table: "Orders");
+        }
+    }
+}
