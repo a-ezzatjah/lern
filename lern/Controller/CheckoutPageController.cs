73ae2f3d66724832c13ed6f12b298bbd68bbbd32
a-ezzatjah <a.ezzatjah@gmail.com>
2026-09-09 22:35:32 +0330

    درست کردن chekout

--- lern/Controller/CheckoutPageController.cs
+++ lern/Controller/CheckoutPageController.cs
@@ -4,5 +4,8 @@ namespace lern.Controller;
 
 public class CheckoutController : Microsoft.AspNetCore.Mvc.Controller
 {
+    // Checkout is data-driven (cart/address are loaded per customer cookie),
+    // so an old browser/proxy response must never be reused for this page.
+    [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Index() => View();
 }
